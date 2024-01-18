835a6ca3f733bc351440fa8eaabb93bf675a038e
WPGuide.at <153843858+WPGuideAT@users.noreply.github.com>
2024-01-18 13:11:13 +0100

    functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
 function hello_elementor_child_scripts_styles() {
 
 	wp_enqueue_style(
@@ -11,3 +21,4 @@ function hello_elementor_child_scripts_styles() {
 		HELLO_ELEMENTOR_CHILD_VERSION
 	);
 }
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
